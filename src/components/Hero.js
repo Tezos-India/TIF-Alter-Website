4679da8a6fd4996171cbbe928ed75d1830afefb2
Tejaaswini <tejnaren07@gmail.com>
2020-11-27 13:27:05 +0530

    update header

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import heroStyles from '../styles/Hero.module.css'
+import Header from './Header'
+
+function Hero() {
+    return (
+        <div className={heroStyles.container}>
+            <Header />
+            
+        </div>
+    )
+}
+
+export default Hero
